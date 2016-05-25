37d83941648905eca1c712b033324ba3217c43bc
mueenie <3828596@qq.com>
2016-05-25 09:34:57 +0800

    doGet

--- src/main/java/test/UploadFileServlet.java
+++ src/main/java/test/UploadFileServlet.java
@@ -27,6 +27,9 @@ public class UploadFileServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		PrintWriter out = response.getWriter();
+		out.print("^_^, get?");
+		out.close();
 	}
 
 	/**
